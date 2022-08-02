35b0a4af3061518f75fe87e2b65d2c5d0b70d586
PlitRoyce <danielmglbraga@gmail.com>
2022-08-02 17:14:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function verificaPalindromo(string) {
+    if(!string) return "string inexistente";
+
+    return string.split("").reverse().join("") === string;
+}
+
+// solução 2
+
+function verificaPalindromo2(string) {
+    if (!string) return "string inexistente";
+
+    for(let i = 1; i < string.length / 2; i++) {
+        if(string[i] !== string[string.length -1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(verificaPalindromo("aboy"));
